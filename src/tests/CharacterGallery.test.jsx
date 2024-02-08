c8279c5ee641a81cc9e277fe5faf4bdc9ec16737
Setsu-Adachi <setsmob@gmail.com>
2024-02-08 20:57:02 +0000

    initial commit

---
+++ src/tests/CharacterGallery.test.jsx
@@ -0,0 +1,6 @@
+import { render } from '@testing-library/react';
+import CharacterGallery from '../components/CharacterGallery'
+
+it('CharacterGallery Component Renders Without Error', () => {
+    render(<CharacterGallery />);
+});
