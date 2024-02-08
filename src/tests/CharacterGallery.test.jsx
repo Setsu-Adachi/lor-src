89453852d185f1b70d041786757c9446ea0f926d
Setsu-Adachi <setsmob@gmail.com>
2024-02-08 21:04:20 +0000

    removed files and kept dist folder

--- src/tests/CharacterGallery.test.jsx
+++
@@ -1,6 +0,0 @@
-import { render } from '@testing-library/react';
-import CharacterGallery from '../components/CharacterGallery'
-
-it('CharacterGallery Component Renders Without Error', () => {
-    render(<CharacterGallery />);
-});
